c6828efd40297b153a1bce16bf3143bb6d368c8e
v.kazarinov <v.kazarinov@cardpay.com>
2019-03-05 17:15:57 +1000

    create components (hoc, context, error)

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as style from './error-indicator.module.css';
+
+const ErrorIndicator = () => {
+  return (<div className={style.indicator}>Error Indicator</div>)
+};
+
+export default ErrorIndicator;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as style from './spinner.module.css';
+
+const Spinner = () => {
+  return (<div className={style.spinner}>Spinner! Loading!</div>)
+};
+
+export default Spinner;
