697fa333b741c9c297417f2a4e932637ff4d97ae
v.kazarinov <v.kazarinov@cardpay.com>
2019-03-06 11:12:14 +1000

    completed settings get data from service and store to book list

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
+import { connect } from 'react-redux';
+import { withBookstoreService } from '../hoc';
+import {booksLoaded} from '../../actions';
+import { compose } from '../../utils';
+
 import * as style from './book-list.module.css';
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1. receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+
+        // 2. dispatch action to store
+        this.props.booksLoaded(data);
+    }
+
     render() {
         const { books } = this.props;
         return (
@@ -19,4 +34,15 @@ class BookList extends Component {
     }
 }
 
-export default BookList;
+const mapStateToProps = ({books}) => {
+   return { books }
+};
+
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
