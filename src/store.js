c7715a4b0c3459b843a7c402f96842e8541d9572
V.Kazarinov <satanigofplains@gmail.com>
2019-03-07 23:48:04 +1000

    add log and string middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import { createStore } from "redux";
+import {createStore, applyMiddleware} from "redux";
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+  console.log(action.type, store.getState());
+  return dispatch(action)
+};
+
+const stringMiddleware = () => (dispatch) => (action) => {
+    if (typeof action === 'string') {
+        return dispatch({
+            type: action
+        })
+    }
+    return dispatch(action)
+};
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 
 export default store;
